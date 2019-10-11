17a9d91b8fc828d6ae90e2fba1891b7ebd2a02e2
Stephen Vector <stephenvector@pm.me>
2019-10-11 13:58:10 -0400

    Setting up jest & enzyme tests.

---
+++ src/__tests__/ArrowLeft.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { mount } from "enzyme";
+import { ArrowLeft } from "../";
+
+describe("<ArrowLeft />", () => {
+  it("Should render", () => {
+    mount(<ArrowLeft />);
+  });
+});
