f11168d4718234180ccf4199f6758fdd704f9883
Stephen Vector <stephenvector@pm.me>
2019-10-12 13:28:04 -0400

    Adding some basic tests/.

--- src/__tests__/ArrowLeft.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { mount } from "enzyme";
-import { ArrowLeft } from "../";
-
-describe("<ArrowLeft />", () => {
-  it("Should render", () => {
-    mount(<ArrowLeft />);
-  });
-});
---
+++ src/__tests__/index.test.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { mount } from "enzyme";
+import {
+  AlignCenter,
+  AlignJustify,
+  AlignLeft,
+  AlignRight,
+  ArrowDown,
+  ArrowLeft,
+  ArrowRight,
+  ArrowUp,
+  Cancel,
+  DragHandle,
+  Menu,
+  Minus,
+  Move,
+  Plus,
+  RadioInactive,
+  RadioActive
+} from "../";
+
+describe("<AlignCenter />", () => {
+  it("Should render", () => {
+    mount(<AlignCenter />);
+  });
+});
+
+describe("<AlignJustify />", () => {
+  it("Should render", () => {
+    mount(<AlignJustify />);
+  });
+});
+
+describe("<AlignLeft />", () => {
+  it("Should render", () => {
+    mount(<AlignLeft />);
+  });
+});
+
+describe("<AlignRight />", () => {
+  it("Should render", () => {
+    mount(<AlignRight />);
+  });
+});
+
+describe("<ArrowDown />", () => {
+  it("Should render", () => {
+    mount(<ArrowDown />);
+  });
+});
+
+describe("<ArrowLeft />", () => {
+  it("Should render", () => {
+    mount(<ArrowLeft />);
+  });
+});
+
+describe("<ArrowRight />", () => {
+  it("Should render", () => {
+    mount(<ArrowRight />);
+  });
+});
+
+describe("<ArrowUp />", () => {
+  it("Should render", () => {
+    mount(<ArrowUp />);
+  });
+});
+
+describe("<Cancel />", () => {
+  it("Should render", () => {
+    mount(<Cancel />);
+  });
+});
+
+describe("<DragHandle />", () => {
+  it("Should render", () => {
+    mount(<DragHandle />);
+  });
+});
+
+describe("<Menu />", () => {
+  it("Menu", () => {
+    mount(<Menu />);
+  });
+});
+
+describe("<Minus />", () => {
+  it("Minus", () => {
+    mount(<Minus />);
+  });
+});
+
+describe("<Move />", () => {
+  it("Should render", () => {
+    mount(<Move />);
+  });
+});
+
+describe("<Plus />", () => {
+  it("Should render", () => {
+    mount(<Plus />);
+  });
+});
+
+describe("<RadioInactive />", () => {
+  it("Should render", () => {
+    mount(<RadioInactive />);
+  });
+});
+
+describe("<RadioActive />", () => {
+  it("Should render", () => {
+    mount(<RadioActive />);
+  });
+});
